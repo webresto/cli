4d22e12d5e2354198255c502c7cff8248a52b2ba
Siziy Ivan <cziy@ya.ru>
2021-07-31 08:49:10 +0300

    generate module worked

--- src/bin/generate.ts
+++ src/bin/generate.ts
@@ -1,30 +1,43 @@
-import fs from 'fs'; 
-import {exec, config} from 'async-shelljs';
-config.silent = true; 
+import fs from 'fs';
+import { exec, config } from 'async-shelljs';
 export const generate = async (what: any, name: any) => {
+    // if (debug) {
+    config.silent = false;
+    // } else {
+    //     config.silent = true;
+    // }
 
-    let modulesPath = process.cwd()+ "/modules/"
-    if (! fs.existsSync(modulesPath)) throw `Dirrectory ${modulesPath} not found`
+    let modulesPath = process.cwd() + '/modules/';
+    if (!fs.existsSync(modulesPath))
+        throw `Dirrectory ${modulesPath} not found`;
 
-    let currentModulePath = process.cwd()+ "/modules/" + name
-    if (fs.existsSync(currentModulePath)) throw `Dirrectory ${currentModulePath} was found`
+    let currentModulePath = process.cwd() + '/modules/' + name;
+    if (fs.existsSync(currentModulePath))
+        throw `Dirrectory ${currentModulePath} was found`;
 
     switch (what) {
-      case "module":
-          let gitExist: boolean = await exec("git --version").code === 0  ? true : false; 
-          if (gitExist) {
-            await exec(`git clone https://github.com/webresto/module-starter.git ${currentModulePath}`)
-            await exec(`${currentModulePath}/utils/module_init`)
-          } else {
-            // await exec(`wget https://github.com/webresto/module-starter/archive/refs/heads/main.zip -O ${currentModulePathTMP} ..... `)
-            console.log("git is not exist")
-          }
-          
-        break;
-    
-      default:
-           throw `Generate ${what} not known`
-        break;
+        case 'module':
+            let gitExist: boolean =
+                (await exec('git --version').code) === 0 ? true : false;
+            if (gitExist) {
+                await exec(
+                    `git clone https://github.com/webresto/module-starter.git ${currentModulePath}`
+                );
+
+                await exec(`rm -rf  ${currentModulePath}/.git`);
+            } else {
+                // await exec(`wget https://github.com/webresto/module-starter/archive/refs/heads/main.zip -O ${currentModulePathTMP} ..... `)
+                console.log('git is not exist');
+            }
+            await exec(
+                `cd ${currentModulePath}/.factory && npm i && cd ${currentModulePath} && .factory/module_init ${name}`
+            );
+
+            break;
+
+        default:
+            throw `Generate ${what} not known`;
+            break;
     }
     console.log(process.cwd(), what, name);
 };
