016e43feec350410b25a6ead01b6b26b050b0033
Siziy Ivan <cziy@ya.ru>
2021-08-16 20:38:29 +0300

    add update npm i

--- src/bin/update.ts
+++ src/bin/update.ts
@@ -62,7 +62,7 @@ export const update = async (module: any) => {
             }
 
             await exec(
-                `set -x; curl https://registry.webresto.dev/${module}/${module}.tar.gz  | tar -xvz -C ${currentModulePath}`
+                `set -x; curl https://registry.webresto.dev/${module}/${module}.tar.gz  | tar -xvz -C ${currentModulePath} && cd ${currentModulePath} && npm i`
             );
         }
     }
