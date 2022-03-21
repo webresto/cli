a2eeb6dcf142992488156fbdbeb601beb8aefbc6
Xziy <x@42.pub>
2022-03-22 01:42:18 +0300

    add tab excpetion for backespace

--- src/bin/repl.ts
+++ src/bin/repl.ts
@@ -5,10 +5,9 @@ var http = require('http');
 var https = require('https');
 
 const readline = require('readline');
-
+var currentLine = ""
 export const repl = async (target: any, options?: any) => {
-
-    if (!target) throw "URL required"
+    if (!target) throw 'URL required';
 
     const opts = url.parse(target);
     opts.method = 'PUT';
@@ -16,13 +15,13 @@ export const repl = async (target: any, options?: any) => {
         Expect: '100-continue'
     };
 
-    let protocol = (new URL(target)).protocol
-    let client = (protocol == "https:") ? https : http
+    let protocol = new URL(target).protocol;
+    let client = protocol == 'https:' ? https : http;
 
     const req = client.request(opts);
 
     opts.agent = new client.Agent({ keepAlive: true });
-    
+
     req.on('response', res => {
         res.pipe(process.stdout);
         res.on('end', function() {
@@ -32,15 +31,39 @@ export const repl = async (target: any, options?: any) => {
 
     const rl = readline.createInterface({
         input: process.stdin,
-        output: process.stdout
+        output: process.stdout,
+        terminal:true
     });
 
     rl.on('line', input => {
+        currentLine = ""
         req.write(input);
         req.write('\n');
     });
 
     rl.on('close', () => {
         process.exit(0);
+    });
+
+    /**
+     * I want to release option to send special code like @tab${command}
+     * for ask REPL-server to autocomplete
+     * 
+     * //TODO:  currently need exception for left and rigth arrow (move cursor)
+     */
+    process.stdin.on('keypress', (chunk, key) => {
+        if (key.name === 'tab') {
+            // console.log("tab")
+            console.log(`@tab${currentLine}`)
+            //req.write(`@tab${currentLine}`);
+            
+        } else if(key.name=== 'backspace') {
+            currentLine = currentLine.slice(0, -1) 
+        } else if(key.name === 'left'){
+            currentLine += key.name
+        } else if(key.name.length === 1){
+            currentLine += key.name
+        }
+        // console.log(key.name)
     });
 };
